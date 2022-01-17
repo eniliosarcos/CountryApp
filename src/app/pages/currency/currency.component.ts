f1624c763f65fa26470d5399b0c1085081a94eb0
eniliosarcos <eniliosarcosdev@gmail.com>
2022-01-17 12:20:46 -0400

    currency exchange added

--- src/app/pages/currency/currency.component.ts
+++ src/app/pages/currency/currency.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { combineLatest, concatMap, distinctUntilChanged, forkJoin, map, switchMap, tap } from 'rxjs';
+import { CountryService } from '../core/services/country.service';
+import { CurrencyService } from './services/currency.service';
+
+interface currency{
+
+}
 
 @Component({
   selector: 'app-currency',
@@ -7,7 +14,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrencyComponent implements OnInit {
 
-  constructor() { }
+  currenciesName$ = this.countryService.country$.pipe(
+    map(country =>  Object.values(country.currencies!).map(currency => currency.name)),
+  )
+
+  currencies$ = this.countryService.country$.pipe(
+    map(country =>  Object.keys(country.currencies!)),
+  )
+
+  currenciesConverted$ = this.countryService.country$.pipe(
+    map(country =>  Object.keys(country.currencies!)),
+    concatMap(currencies => {
+      const observables = currencies.map(currency => this.currencyService.getConvertedCurrencyValue(currency));
+      return forkJoin([...observables]);
+    }),
+    map(resp => resp.map(a => Object.values(a))),
+  )
+
+
+
+
+  constructor(private currencyService: CurrencyService, private countryService: CountryService) { }
 
   ngOnInit(): void {
   }
