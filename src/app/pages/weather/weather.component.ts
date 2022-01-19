39d44157de3344c67b5eadbb5745e98d7133f406
eniliosarcos <eniliosarcosdev@gmail.com>
2022-01-19 15:13:59 -0400

    improvements in the styles of the description component

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -6,19 +6,23 @@ import { WeatherService } from './services/weather.service';
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.scss']
+  styleUrls: ['./weather.component.scss'],
 })
 export class WeatherComponent implements OnInit {
-
   weatherData$ = this.countryService.country$.pipe(
-    filter(country => !!Boolean(country)),
-    switchMap(resp => this.weatherService.getWeatherData(resp!.latlng[0],resp!.latlng[1]))
-    // map(({articles}) => articles)
-  )
+    filter((country) => !!Boolean(country)),
+    switchMap((resp) =>
+      this.weatherService.getWeatherData(resp!.latlng[0], resp!.latlng[1])
+    )
+  );
 
-  constructor(private countryService: CountryService, private weatherService:WeatherService) { }
+  constructor(
+    private countryService: CountryService,
+    private weatherService: WeatherService
+  ) {}
 
-  ngOnInit(): void {
+  iconWeather(icon: string): string {
+    return `http://openweathermap.org/img/w/${icon}.png`;
   }
-
+  ngOnInit(): void {}
 }
