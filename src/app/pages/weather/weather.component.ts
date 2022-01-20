f80d1ae4037f2db583c2adde2444ea61082fcf09
eniliosarcos <eniliosarcosdev@gmail.com>
2022-01-20 04:57:39 -0400

    weather with loading when data is emptly

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { filter, map, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, debounceTime, delay, filter, map, switchMap, tap } from 'rxjs';
 import { CountryService } from '../core/services/country.service';
 import { WeatherService } from './services/weather.service';
 
@@ -9,15 +9,23 @@ import { WeatherService } from './services/weather.service';
   styleUrls: ['./weather.component.scss'],
 })
 export class WeatherComponent implements OnInit {
-  weatherData$ = this.countryService.country$.pipe(
+
+  loading$ = new BehaviorSubject<boolean>(false);
+  country$ = this.weatherService.country$.pipe(
+    tap(x => console.log(x))
+  );
+
+  weatherData$ = this.country$.pipe(
     filter((country) => !!Boolean(country)),
-    switchMap((resp) =>
-      this.weatherService.getWeatherData(resp!.latlng[0], resp!.latlng[1])
-    )
+    tap(() => this.loading$.next(true)),
+    delay(1000),
+    switchMap((resp) => {
+      return this.weatherService.getWeatherData(resp!.latlng[0], resp!.latlng[1])
+    }),
+    tap(() => this.loading$.next(false))
   );
 
   constructor(
-    private countryService: CountryService,
     private weatherService: WeatherService
   ) {}
 
