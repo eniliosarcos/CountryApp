b0553d0bf3f935b05884765ad3491679b0e12175
eniliosarcos <eniliosarcosdev@gmail.com>
2022-01-20 16:57:56 -0400

    the circle now snaps to the spot on the map.

--- src/app/shared/map/map.component.ts
+++ src/app/shared/map/map.component.ts
@@ -13,48 +13,57 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnChanges, AfterViewInit {
-  @Input() latlng!:number[];
+  @Input() latlng!: number[];
 
-  private map:any;
+  private map: any;
+  circle:any;
 
-  constructor(){
-
-  }
+  constructor() {}
   ngOnChanges(changes: SimpleChanges): void {
-    if(this.latlng){
-      this.changeView(this.latlng[0],this.latlng[1])
+    if (this.latlng) {
+      this.changeView(this.latlng[0], this.latlng[1]);
     }
   }
 
-  changeView(lat:any,lng:any) {
-    this.map.flyTo(new L.LatLng(lat, lng),this.map.getZoom(),{
-      "animate":true,
-      "pan":{
-        "duration":1
-      }
-    })
-}
+  changeView(lat: any, lng: any) {
+    this.map.removeControl(this.circle);
+    this.map.flyTo(new L.LatLng(lat, lng), this.map.getZoom(), {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+    this.circle = L.circle([lat, lng], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 30000,
+    }).addTo(this.map);
+  }
 
   ngAfterViewInit(): void {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(this.setGeoLocation.bind(this));
-   }
+    }
   }
 
   setGeoLocation(position: { coords: { latitude: any; longitude: any } }) {
     const {
-       coords: { latitude, longitude },
+      coords: { latitude, longitude },
     } = position;
 
     this.map = L.map('map').setView([latitude, longitude], 5);
 
-
-
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-     attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>contributors'
-     } ).addTo(this.map);
+      attribution:
+        '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>contributors',
+    }).addTo(this.map);
 
-     L.circle([latitude, longitude], 30000).addTo(this.map)
- }
-
+    this.circle = L.circle([latitude, longitude], {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5,
+      radius: 20000,
+    }).addTo(this.map);
+  }
 }
